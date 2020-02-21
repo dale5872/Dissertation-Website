e08a60b05b7fa74145ec780981931860f8474099
Dale Baker-Allan <daleb1206@gmail.com>
2020-02-21 20:12:45 +0000

    Create Questionnaire Form Complete

--- client/src/app/dashboard/create-questionnaire/create-questionnaire.component.ts
+++ client/src/app/dashboard/create-questionnaire/create-questionnaire.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from 'src/app/_service/http.service';
+import { BootstrapAlertService } from 'ngx-bootstrap-alert-service'
+import { FormControl, FormGroup, FormBuilder, FormArray, Form } from '@angular/forms';
+
 
 @Component({
   selector: 'app-create-questionnaire',
@@ -6,10 +10,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-questionnaire.component.css']
 })
 export class CreateQuestionnaireComponent implements OnInit {
+  questionnaireInfoForm: FormGroup;
+  questions: FormArray;
 
-  constructor() { }
+  constructor(    
+    private http: HttpService,
+    private alertService: BootstrapAlertService,
+    private formBuilder: FormBuilder
+  ) {
+    this.questionnaireInfoForm = this.createQuestionnaireForm();
+   }
 
   ngOnInit() {
+
   }
 
+  private createQuestionnaireForm(): FormGroup {
+    return this.formBuilder.group({
+      questionnaireInfo: this.formBuilder.group({
+        questionnaireInfoData: ''
+      }),
+      questions: this.formBuilder.array([this.createQuestion()])
+    });
+  }
+
+  private createQuestion(): FormGroup {
+    return this.formBuilder.group({
+      questionName: ''
+    });
+  }
+
+  private addQuestion(): void {
+    this.questions = this.questionnaireInfoForm.get('questions') as FormArray;
+    this.questions.push(this.createQuestion());
+  }
 }
